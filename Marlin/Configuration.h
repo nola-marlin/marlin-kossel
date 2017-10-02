86282ce00119672f504308f04af7e1833ee96d7d
Chris Spring <chris@nolaspring.net>
2017-10-02 14:14:00 -0400

    dropped smooth rod offset to compensate for convex movment

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -444,10 +444,10 @@
 
   // Horizontal offset from middle of printer to smooth rod center.
   // correcting flatness: increasing lowers hotend
-  #define DELTA_SMOOTH_ROD_OFFSET 130.0 // mm
+  #define DELTA_SMOOTH_ROD_OFFSET 120.0 // mm
 
   // Horizontal offset of the universal joints on the end effector.
-  #define DELTA_EFFECTOR_OFFSET 30 // mm
+  #define DELTA_EFFECTOR_OFFSET 30.0 // mm
 
   // Horizontal offset of the universal joints on the carriages.
   #define DELTA_CARRIAGE_OFFSET 20.6 // mm
